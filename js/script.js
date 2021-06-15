011622c546f141c20347d7034a41dd5817181b4b
pHedi3 <federico.gualdi3@gmail.com>
2021-06-15 16:06:28 +0200

    fine es

--- js/script.js
+++ js/script.js
@@ -8,3 +8,42 @@ var student = {
 for (var k in student) {
     document.querySelector('.result').innerHTML += k + ': ' + student[k] + '<br>';
 }
+
+//parte due
+
+var school = [
+    {nome: 'timo',
+    surname: 'bagno',
+    age: 20
+    },
+    {nome: 'palo',
+    surname: 'nando',
+    age: 14
+    },
+    {nome: 'rugo',
+    surname: 'saldo',
+    age: 26
+    },
+];
+
+for (var x = 0; x < school.length; x++) {
+    document.getElementById('output').innerHTML += school[x].nome + ' ' + school[x].surname + '<br>'
+}
+
+// parte tre
+
+var userName = prompt('inserisci il nome');
+var userSurname = prompt('inserisci il cognome');
+var userAge = prompt('insersci la tua età')
+
+school.push({
+    nome: userName,
+    surname: userSurname,
+    age: userAge
+})
+
+
+
+for (var x = 0; x < school.length; x++) {
+    document.getElementsByClassName('fine')[0].innerHTML += school[x].nome + ' ' + school[x].surname + '<br>'
+}
